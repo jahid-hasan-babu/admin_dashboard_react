f1ddb0c7bd535c2de9a3dcfd721b3825280e784d
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2023-10-08 00:13:11 +0600

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Layout from './components/Layout/Layout'
+import Calendar from './pages/Calendar/Calendar'
+import Board from './pages/Board/Board'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='dashboard'>
+        <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Layout/>}>
+
+          <Route path="dashboard" element={<Dashboard/>}/>
+          <Route path="calendar" element={<Calendar/>}/>
+          <Route path="board" element={<Board/>}/>
+        </Route>
+        
+
+      </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
